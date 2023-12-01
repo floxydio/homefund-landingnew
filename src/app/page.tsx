4bcce08a641fc772e41706123d9543ba399a333a
floxydio <diooktar@gmail.com>
2023-12-02 02:38:16 +0700

    update color and ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/Components/HeaderSection/Header.section";
+import Simulasi from "@/Components/SimulasiSection/Simulasi.section";
 
 
 export default function Home() {
   return (
     <div>
       <Header />
+      {/* <Simulasi /> */}
     </div>
   )
 }
