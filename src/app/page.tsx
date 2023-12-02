c4cfee206bb4a8b6aeef7924939f043ea187df40
floxydio <diooktar@gmail.com>
2023-12-02 15:20:10 +0700

    update feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Feature from "@/Components/FeatureSection/Feature";
 import Header from "@/Components/HeaderSection/Header.section";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <Header />
+      <Feature/>
     </div>
   )
 }
