736559be9b4032f121356e13c5ae86dc13e69417
marcellinoputra <marcellinoputra@github.com>
2023-12-19 18:49:33 +0700

    update tentang kami

--- src/Components/HeaderSection/widget/Navbar.tsx
+++ src/Components/HeaderSection/widget/Navbar.tsx
@@ -1,45 +1,67 @@
-import Link from "next/link";
-import { FiAlignCenter, FiPlay } from "react-icons/fi";
+import Link from 'next/link';
+import { FiAlignCenter, FiPlay } from 'react-icons/fi';
 
 export default function Navbar() {
-    return (
-        <>
-            <div className="hidden lg:block">
-                <div className="navbar flex flex-row justify-between items-center mb-[50px]">
-                    <div className="left flex items-center">
-                        <h2 className="text-2xl font-extrabold pr-[32px]">HomeFunding</h2>
-                        <span className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]">Fitur</span>
-                        <span className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]">Product</span>
-
-                        <span className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]">Blog</span>
-                        <span className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]">Tentang Kami</span>
-                        <Link className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]" href="/career">Karir</Link>
-                    </div>
-                    <div className="right flex flex-row items-center h-full">
-                        {/* Button daftar */}
-                        {/* <div className="bg-[#4169E1] w-[100px] text-white text-center h-[40px] rounded-[8px] flex justify-center items-center">
+  return (
+    <>
+      <div className="hidden lg:block">
+        <div className="navbar flex flex-row justify-between items-center mb-[50px]">
+          <div className="left flex items-center">
+            <h2 className="text-2xl font-extrabold pr-[32px]">HomeFunding</h2>
+            <span className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]">
+              Fitur
+            </span>
+            <span className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]">
+              Product
+            </span>
+            <span className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]">
+              Blog
+            </span>
+            <Link
+              className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]"
+              href="/tentang-kami"
+            >
+              Tentang Kami
+            </Link>
+            <Link
+              className="mr-[15px] font-semibold hover:cursor-pointer hover:text-[#4169E1]"
+              href="/career"
+            >
+              Karir
+            </Link>
+          </div>
+          <div className="right flex flex-row items-center h-full">
+            {/* Button daftar */}
+            {/* <div className="bg-[#4169E1] w-[100px] text-white text-center h-[40px] rounded-[8px] flex justify-center items-center">
                             <span className="text-white font-bold">Daftar</span>
                         </div> */}
-
-                    </div>
-                </div>
-            </div>
-            {/* Mobile Version */}
-            <div className="lg:hidden">
-                <div className="navbar flex flex-row justify-between items-center">
-                    <div className="left lg:flex items-center">
-                        <h2 className="text-2xl font-bold pr-[32px]">HomeFunding</h2>
-                        <span className="mr-[15px] font-semibold hover:cursor-pointer hidden">Fitur</span>
-                        <span className="mr-[15px] font-semibold hover:cursor-pointer hidden">Product</span>
-                        <span className="mr-[15px] font-semibold hover:cursor-pointer hidden">Blog</span>
-                        <span className="mr-[15px] font-semibold hover:cursor-pointer hidden ">Bantuan</span>
-                    </div>
-                    <div className="right">
-                        <FiAlignCenter className="text-2xl hover:cursor-pointer" />
-                    </div>
-                </div>
-            </div>
-            {/*  */}
-        </>
-    )
+          </div>
+        </div>
+      </div>
+      {/* Mobile Version */}
+      <div className="lg:hidden">
+        <div className="navbar flex flex-row justify-between items-center">
+          <div className="left lg:flex items-center">
+            <h2 className="text-2xl font-bold pr-[32px]">HomeFunding</h2>
+            <span className="mr-[15px] font-semibold hover:cursor-pointer hidden">
+              Fitur
+            </span>
+            <span className="mr-[15px] font-semibold hover:cursor-pointer hidden">
+              Product
+            </span>
+            <span className="mr-[15px] font-semibold hover:cursor-pointer hidden">
+              Blog
+            </span>
+            <span className="mr-[15px] font-semibold hover:cursor-pointer hidden ">
+              Bantuan
+            </span>
+          </div>
+          <div className="right">
+            <FiAlignCenter className="text-2xl hover:cursor-pointer" />
+          </div>
+        </div>
+      </div>
+      {/*  */}
+    </>
+  );
 }
--- src/app/tentang-kami/page.tsx
+++ src/app/tentang-kami/page.tsx
@@ -1,9 +1,120 @@
+import Footer from '@/Components/FooterSection/FooterSection';
+import Navbar from '@/Components/HeaderSection/widget/Navbar';
+import Link from 'next/link';
+
 export default function Page() {
-    return (
-        <>
-            <h2>Tentang Kami Page</h2>
+  return (
+    <>
+      <div className="lg:ml-[112px] lg:mr-[112px] lg:mt-[24px]  ml-[40px] mr-[40px] mt-[40px]">
+        <Navbar />
+        <h1 className="text-3xl text-center text-[#4169E1] font-sans ">
+          Yuk Kenalan
+        </h1>
+        <p className="text-[18px] mt-[10px] text-center font-sans">
+          Aplikasi investasi berbasis crowd funding untuk investasi rumah dan
+          bisnis untuk semua orang
+        </p>
+        <h1 className="mt-10 text-lg font-sans">Mari Mulai Dengan,</h1>
+        <h1 className="text-2xl font-sans">Apa Itu Homefunding?</h1>
+        <div className="mt-5 flex-wrap md:justify-center font-sans">
+          <p className="text-justify mb-2 font-sans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            distinctio minima earum doloremque rem ipsum iusto unde id. Magni
+            laudantium eius accusamus ad deserunt id praesentium recusandae
+            architecto delectus eaque.
+          </p>
+          <p className="text-justify mb-2 font-sans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            distinctio minima earum doloremque rem ipsum iusto unde id. Magni
+            laudantium eius accusamus ad deserunt id praesentium recusandae
+            architecto delectus eaque.
+          </p>
+          <p className="text-justify mb-2 font-sans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            distinctio minima earum doloremque rem ipsum iusto unde id. Magni
+            laudantium eius accusamus ad deserunt id praesentium recusandae
+            architecto delectus eaque.
+          </p>
+        </div>
 
+        <div>
+          <p className="font-black text-6xl font-sans">
+            Sejak 2018, pengguna setia kami telah mempercayakan kami dengan 10
+            triliun+ rupiah transaksi
+          </p>
+        </div>
+      </div>
+      <div className="bg-[#4169E1] mt-10 rounded">
+        <p className="text-3xl text-center text-white mb-5 font-sans pb-10 pt-10">
+          Pimpinan Kami
+        </p>
+        <div className="lg:ml-[112px] lg:mr-[112px] lg:mt-[24px]  ml-[40px] mr-[40px] mt-[40px]">
+          <div className="grid grid-cols-3 gap-3 mr-5 ml-5 pb-10 mb-10">
+            {/* Ini Dio Okta */}
+            <div className="max-w-sm  overflow-hidden shadow-lg bg-white rounded">
+              <img
+                className="w-full bg-white"
+                src="/Profile/Dio.png"
+                alt="CTO Homefunding"
+              />
+              <div className="px-6 py-4 bg-white">
+                <div className="font-bold text-xl mb-2 text-center font-sans">
+                  Dio Okta Rovelino
+                </div>
+                <p className="text-gray-700 text-base text-center font-sans">
+                  Chief Technology Officer
+                </p>
+              </div>
+            </div>
 
-        </>
-    )
+            {/* Ini Juli Huang */}
+            <div className="max-w-sm  overflow-hidden shadow-lg bg-white rounded">
+              <img
+                className="w-full bg-white"
+                src="/Profile/Ilung.png"
+                alt="CTO Homefunding"
+              />
+              <div className="px-6 py-4 bg-white">
+                <div className="font-bold text-xl mb-2 text-center font-sans">
+                  Juli Huang Ilung
+                </div>
+                <p className="text-gray-700 text-base text-center font-sans">
+                  Chief Executive Officer
+                </p>
+              </div>
+            </div>
+
+            {/* Ini Blek */}
+            <div className="max-w-sm  overflow-hidden shadow-lg bg-white rounded">
+              <img
+                className="w-full bg-white"
+                src="/Profile/Ramanda.png"
+                alt="COO Homefunding"
+              />
+              <div className="px-6 py-4 bg-white">
+                <div className="font-bold text-xl mb-2 text-center font-sans">
+                  Ramanda Dio
+                </div>
+                <p className="text-gray-700 text-base text-center font-sans">
+                  Chief Operating Officer
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[300px] h-[100px] ml-5">
+        <p className="text-justify pb-3">Gabung Bersama Kami!</p>
+        <button>
+          <Link
+            className="px-4 py-2 font-bold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
+            href="/career"
+          >
+            Lowongan Tersedia
+          </Link>
+        </button>
+      </div>
+      <Footer />
+    </>
+  );
 }
