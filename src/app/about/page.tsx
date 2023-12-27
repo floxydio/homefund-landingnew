faa14d5667a98d5f5ce8834d67c7c83cb74ced50
marcellinoputra <marcellinoputra@github.com>
2023-12-27 14:14:30 +0700

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <>
       <div className="lg:ml-[112px] lg:mr-[112px] lg:mt-[24px]  ml-[40px] mr-[40px] mt-[40px]">
-        {/* <Navbar /> */}
+        <Navbar />
         <div className="background-header-about">
           <h1 className="text-3xl text-center text-[#4169E1] font-sans ">
             Yuk Kenalan
--- tawk.d.ts
+++ tawk.d.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 declare module '@tawk.to/tawk-messenger-react'
-=======
-declare module '@tawk.to/tawk-messenger-react';
->>>>>>> 08b8d02 (.)
